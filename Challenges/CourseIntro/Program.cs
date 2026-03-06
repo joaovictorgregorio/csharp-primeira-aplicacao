9cc52d1f8886cd6bb0ff43eb77795f82c333ca19
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-06 19:33:00 -0300

    feat: criar app console para exibir informações do curso e instrutores
    Desafio realizado utilizando apenas o .NET CLI, sem IDE.
    - Armazenar nome do curso em uma variável
    - Armazenar nome e sobrenome em variáveis
    - Exibir nomes dos instrutores no console

---
+++ Challenges/CourseIntro/Program.cs
@@ -0,0 +1,6 @@
+﻿string curso = "C#: criando sua primeira aplicação";
+Console.WriteLine(curso + "\n");
+
+string nome = "Guilherme Lima";
+string sobrenome = "Daniel Portugal";
+Console.WriteLine($"Instrutores: \n{nome} \n{sobrenome}");
