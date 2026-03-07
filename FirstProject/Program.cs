3f3bebcced51388ae89ab24efd0ce4748e9c2b6a
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-06 22:43:32 -0300

    Adicionado menu de console interativo com saída colorida
    Substituímos o texto básico "Olá, Mundo!" por um título estilizado em ASCII e um menu principal interativo. Adicionamos funções auxiliares para saída de texto colorida e animada. Os usuários agora podem cadastrar bandas, listar bandas, avaliar bandas, visualizar avaliações médias ou sair. Opções inválidas exibem um erro e redefinem o menu para uma melhor experiência do usuário.

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string titleApplication = @"
+▒█▀▀▀█ █▀▀ █▀▀█ █▀▀ █▀▀ █▀▀▄ 　 ▒█▀▀▀█ █▀▀█ █░░█ █▀▀▄ █▀▀▄ 
+░▀▀▀▄▄ █░░ █▄▄▀ █▀▀ █▀▀ █░░█ 　 ░▀▀▀▄▄ █░░█ █░░█ █░░█ █░░█ 
+▒█▄▄▄█ ▀▀▀ ▀░▀▀ ▀▀▀ ▀▀▀ ▀░░▀ 　 ▒█▄▄▄█ ▀▀▀▀ ░▀▀▀ ▀░░▀ ▀▀▀░";
+
+void PrintColored(string text, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(text);
+    Console.ResetColor();
+}
+
+void TypeLine(string text, ConsoleColor color = ConsoleColor.White, int delayMs = 1)
+{
+    Console.ForegroundColor = color;
+    foreach (char c in text)
+    {
+        Console.Write(c);
+        Thread.Sleep(delayMs);
+    }
+
+    Console.WriteLine();
+    Console.ResetColor();
+}
+
+void ShowWelcomeMessage()
+{
+    PrintColored(titleApplication, ConsoleColor.Cyan);
+}
+
+void DisplayMenuOptions()
+{
+    Console.WriteLine();
+    PrintColored("                     MENU PRINCIPAL", ConsoleColor.Yellow);
+    Console.WriteLine();
+
+    TypeLine("                  [1] Registrar banda", ConsoleColor.White);
+    TypeLine("                  [2] Listar todas as bandas", ConsoleColor.White);
+    TypeLine("                  [3] Avaliar uma banda", ConsoleColor.White);
+    TypeLine("                  [4] Ver média de avaliações", ConsoleColor.White);
+    TypeLine("                  [0] Sair", ConsoleColor.Red);
+
+    Console.WriteLine();
+    Console.Write("Escolha uma opção: ");
+    int optionChosen = int.Parse(Console.ReadLine()!);
+
+    switch (optionChosen)
+    {
+        case 1: 
+            Console.WriteLine("1");
+            break;
+
+        case 2: 
+            Console.WriteLine("2"); 
+            break;
+
+        case 3: 
+            Console.WriteLine("3");
+            break;
+
+        case 4: 
+            Console.WriteLine("4");
+            break;
+
+        case 0:
+            PrintColored("Tchau :)", ConsoleColor.DarkYellow);
+            break;
+
+        default:
+            TypeLine("OPÇÃO INVÁLIDA. TENTE NOVAMENTE", ConsoleColor.Red, 30);
+            Console.Clear();
+            ShowWelcomeMessage();
+            DisplayMenuOptions();
+            break;
+    }
+}
+
+
+ShowWelcomeMessage();
+DisplayMenuOptions();
